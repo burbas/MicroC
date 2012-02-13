fe5b614a7b366d18b5154a77e5f093977919cff6
Niclas Axelsson <niclas@burbas.se>
2012-02-13 22:37:30 +0100

    Add helper functions for lexer and parser.

--- test/mrh.erl
+++ test/mrh.erl
@@ -1,9 +1,21 @@
 -module(mrh).
 
--export([helper/2]).
+-export([helper/2, lexer_helper/2, parser_helper/2]).
 
 helper(Path, File) ->
-    case catch microc:compile(Path++File) of
+    case catch microc:analyze(Path++File) of
+	{'EXIT', Error} ->
+	    Error;
+	A ->
+	    A
+    end.
+
+lexer_helper(Path, File) ->
+    microc:lex(Path++File).
+	    
+
+parser_helper(Path, File) ->
+    case catch microc:parse(Path++File) of
 	{'EXIT', Error} ->
 	    Error;
 	A ->
