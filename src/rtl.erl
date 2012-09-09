0596ea3dc57383440c470132b8a78874a3f852fc
Niclas Axelsson <niclas@burbas.se>
2012-09-09 23:57:32 +0200

    Indent the rtl-module correctly

--- src/rtl.erl
+++ src/rtl.erl
@@ -13,15 +13,15 @@
 -export([ast_to_rtl/1]).
 
 -record(state, {
-	  label_counter = 100,
-	  temp_counter = 1,
-	  previous_temp = [],
-	  current_function,
-	  ir = [],
-	  framesize = 0,
-	  symboltable,
-	  fp = 1
-	 }).
+          label_counter = 100,
+          temp_counter = 1,
+          previous_temp = [],
+          current_function,
+          ir = [],
+          framesize = 0,
+          symboltable,
+          fp = 1
+         }).
 
 
 %%% Evaluate variables from function arguments differently
@@ -35,15 +35,15 @@ sizeof(?LONG) -> 4. %% 32-bits representation, 4 bytes
 convert_type({int_constant, _, _, _}) -> ?LONG;
 convert_type({char, _}) -> ?BYTE;
 convert_type({int, _}) -> ?LONG;
-convert_type({void, _}) -> ?BYTE. 
+convert_type({void, _}) -> ?BYTE.
 
 clean(State) -> State#state{ir = []}.
 
 unwrap({'int_constant', _TokenLine, Value}) -> Value;
-unwrap({'ident', _TokenLine, _TokenLen, Value}) -> Value; 
+unwrap({'ident', _TokenLine, _TokenLen, Value}) -> Value;
 unwrap({Value, _TokenLine}) -> Value;
 unwrap(Value) -> Value.
- 
+
 newLabel(State = #state{label_counter = LabelCounter}) ->
     Label = "L" ++ erlang:integer_to_list(LabelCounter),
     {Label, State#state{label_counter = LabelCounter + 1}}.
@@ -58,22 +58,22 @@ insert(Name, Temporary, State = #state{symboltable = ST}) ->
 
 lookup(Name, State = #state{symboltable = ST}) ->
     case dict:find(Name, ST) of
-	{ok, Value} ->
-	    Value;
-	_ ->
-	    {error, not_found}
+        {ok, Value} ->
+            Value;
+        _ ->
+            {error, not_found}
     end.
 
 flatten([], State) ->
     State;
 flatten([#'GLOBAL'{declaration = Declaration}|Tl], State) ->
-    IR = 
-	case Declaration of
-	    #'VARDEC'{base_type = BaseType, declarator = Declarator} ->
-		State#state.ir ++ [#'RTL_DATA'{label = unwrap(Declarator), size = sizeof(convert_type(BaseType))}];
-	    #'ARRDEC'{identifier = Identifier, base_type = BaseType, size = Size} ->
-		State#state.ir ++ [#'RTL_DATA'{label = unwrap(Identifier), size = sizeof(convert_type(BaseType))*unwrap(Size)}] 
-	end,
+    IR =
+        case Declaration of
+            #'VARDEC'{base_type = BaseType, declarator = Declarator} ->
+                State#state.ir ++ [#'RTL_DATA'{label = unwrap(Declarator), size = sizeof(convert_type(BaseType))}];
+            #'ARRDEC'{identifier = Identifier, base_type = BaseType, size = Size} ->
+                State#state.ir ++ [#'RTL_DATA'{label = unwrap(Identifier), size = sizeof(convert_type(BaseType))*unwrap(Size)}]
+        end,
     flatten(Tl, State#state{ir = IR});
 
 %% Local variable declarations
@@ -118,30 +118,30 @@ flatten([#'BINARY_OP'{operation = OP, expression1 = Expr1, expression2 = Expr2}|
     FExpr2 = flatten(Expr2, FExpr1),
 
     %% Binary operations needs to be handled differently
-    BinResult = 
-	case OP of
-	    'minus' ->
-		[#'RTL_EVAL'{dest = Temp, value = #'RTL_SUB'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
-	    'plus' ->
-		[#'RTL_EVAL'{dest = Temp, value = #'RTL_ADD'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
-	    'div' ->
-		[#'RTL_EVAL'{dest = Temp, value = #'RTL_DIV'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
-	    'mul' ->
-		[#'RTL_EVAL'{dest = Temp, value = #'RTL_MUL'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
-	    _ ->
-		[#'RTL_BINARY'{
-		    binop = OP, 
-		    dest = Temp, 
-		    temp1 = FExpr1#state.previous_temp, 
-		    temp2 = FExpr2#state.previous_temp}
-		]
-	end,
-    
-    IR = 
-	State#state.ir ++ 
-	FExpr1#state.ir ++ 
-	FExpr2#state.ir ++ 
-	BinResult,
+    BinResult =
+        case OP of
+            'minus' ->
+                [#'RTL_EVAL'{dest = Temp, value = #'RTL_SUB'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
+            'plus' ->
+                [#'RTL_EVAL'{dest = Temp, value = #'RTL_ADD'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
+            'div' ->
+                [#'RTL_EVAL'{dest = Temp, value = #'RTL_DIV'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
+            'mul' ->
+                [#'RTL_EVAL'{dest = Temp, value = #'RTL_MUL'{temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}}];
+            _ ->
+                [#'RTL_BINARY'{
+                    binop = OP,
+                    dest = Temp,
+                    temp1 = FExpr1#state.previous_temp,
+                    temp2 = FExpr2#state.previous_temp}
+                ]
+        end,
+
+    IR =
+        State#state.ir ++
+        FExpr1#state.ir ++
+        FExpr2#state.ir ++
+        BinResult,
 
     flatten(Tl, State#state{ir = IR, previous_temp = Temp});
 
@@ -151,22 +151,22 @@ flatten([#'UNARY'{operation = 'minus', expression = Expr}|Tl], State) ->
 
     ExprState = flatten(Expr, NewState2),
 
-    IR = 
-	State#state.ir ++ 
-	ExprState#state.ir ++ 
-	[
-	 #'RTL_EVAL'{
-	    dest = Temp1,
-	    value = #'RTL_ICON'{int = 0}
-	   },
-	 #'RTL_EVAL'{
-		      dest = Temp2,
-		      value = #'RTL_SUB'{
-			temp1 = Temp1,
-			temp2 = ExprState#state.previous_temp
-		       }
-		    }
-	],
+    IR =
+        State#state.ir ++
+        ExprState#state.ir ++
+        [
+         #'RTL_EVAL'{
+            dest = Temp1,
+            value = #'RTL_ICON'{int = 0}
+           },
+         #'RTL_EVAL'{
+                      dest = Temp2,
+                      value = #'RTL_SUB'{
+                        temp1 = Temp1,
+                        temp2 = ExprState#state.previous_temp
+                       }
+                    }
+        ],
 
     flatten(Tl, ExprState#state{ir = IR, previous_temp = Temp2});
 
@@ -178,45 +178,45 @@ flatten([#'UNARY'{operation = 'not', expression = Expr}|Tl], State) ->
     {Onelbl, NewState2} = newLabel(NewState),
     {Endlbl, NewState3} = newLabel(NewState2),
 
-    IR = 
-	State#state.ir ++
-	ExprState#state.ir ++
-	[
-	 #'RTL_CJUMP'{
-	    relop = 'eq',
-	    temp1 = ExprState#state.previous_temp,
-	    temp2 = #'RTL_ICON'{int = 0},
-	    label = Onelbl
-	   },
-	 #'RTL_EVAL'{
-		      dest = ExprState#state.previous_temp,
-		      value = #'RTL_ICON'{int = 0}
-		    },
-	 #'RTL_JUMP'{
-		      label = Endlbl
-		    },
-	 #'RTL_LABELDEF'{
-		      label = Onelbl
-		     },
-	 #'RTL_EVAL'{
-		      dest = ExprState#state.previous_temp,
-		      value = #'RTL_ICON'{int = 1}
-		    },
-	 #'RTL_LABELDEF'{
-		      label = Endlbl
-		     }
-	],
+    IR =
+        State#state.ir ++
+        ExprState#state.ir ++
+        [
+         #'RTL_CJUMP'{
+            relop = 'eq',
+            temp1 = ExprState#state.previous_temp,
+            temp2 = #'RTL_ICON'{int = 0},
+            label = Onelbl
+           },
+         #'RTL_EVAL'{
+                      dest = ExprState#state.previous_temp,
+                      value = #'RTL_ICON'{int = 0}
+                    },
+         #'RTL_JUMP'{
+                      label = Endlbl
+                    },
+         #'RTL_LABELDEF'{
+                      label = Onelbl
+                     },
+         #'RTL_EVAL'{
+                      dest = ExprState#state.previous_temp,
+                      value = #'RTL_ICON'{int = 1}
+                    },
+         #'RTL_LABELDEF'{
+                      label = Endlbl
+                     }
+        ],
 
     flatten(Tl, NewState3#state{ir = IR});
 
 flatten([#'ASSIGN'{expr1 = Expr1, expr2 = Expr2}|Tl], State) ->
     {Temp, NewState} = newTemp(State),
-    
+
     FExpr1 = flatten(Expr1, NewState),
     FExpr2 = flatten(Expr2, FExpr1),
-    
+
     IR = State#state.ir ++ FExpr1#state.ir ++ FExpr2#state.ir ++ [#'RTL_ASSIGN'{dest = Temp, temp1 = FExpr1#state.previous_temp, temp2 = FExpr2#state.previous_temp}],
-    
+
     flatten(Tl, State#state{ir = IR, previous_temp = Temp});
 
 flatten([#'IF'{expression = Expr, statement1 = Stmt1, statement2 = Stmt2}|Tl], State) ->
@@ -225,35 +225,36 @@ flatten([#'IF'{expression = Expr, statement1 = Stmt1, statement2 = Stmt2}|Tl], S
 
 
     #'EXPRESSION'{value = #'BINARY_OP'{operation = OP, expression1 = Expr1, expression2 = Expr2}} = Expr,
-    
+
     Expr1State = flatten(Expr1, NewState),
     Expr2State = flatten(Expr2, Expr1State),
-    
+
     Stmt1State = flatten(Stmt1, Expr2State),
     Stmt2State = flatten(Stmt2, Stmt1State),
-    
-    IR = 
-	State#state.ir ++ 
-	Expr1State#state.ir ++ 
-	Expr2State#state.ir ++  
-	[#'RTL_CJUMP'{
-		      relop = unwrap(OP),
-		      temp1 = Expr1State#state.previous_temp,
-		      temp2 = Expr2State#state.previous_temp,
-		      label = ElseLabel
-		    },
-	 Stmt1#state.ir,
-	 #'RTL_JUMP'{
-		      label = EndLabel
-		    },
-	 #'RTL_LABELDEF'{
-		      label = ElseLabel
-		     }] ++
-	Stmt2#state.ir ++
-	[#'RTL_LABELDEF'{
-	    label = EndLabel
-	   }],
-    
+
+    IR =
+        State#state.ir ++
+        Expr1State#state.ir ++
+        Expr2State#state.ir ++
+        [#'RTL_CJUMP'{
+            relop = unwrap(OP),
+            temp1 = Expr1State#state.previous_temp,
+            temp2 = Expr2State#state.previous_temp,
+            label = ElseLabel
+           },
+
+         Stmt1#state.ir,
+         #'RTL_JUMP'{
+                      label = EndLabel
+                    },
+         #'RTL_LABELDEF'{
+                      label = ElseLabel
+                     }] ++
+        Stmt2#state.ir ++
+        [#'RTL_LABELDEF'{
+            label = EndLabel
+           }],
+
     flatten(Tl, State#state{ir = IR, previous_temp = []});
 
 flatten([#'WHILE'{expression = Expr, statement = Stmt}|Tl], State) ->
@@ -264,24 +265,24 @@ flatten([#'WHILE'{expression = Expr, statement = Stmt}|Tl], State) ->
     ExprState = flatten(Expr, NewState3),
 
     IR = [#'RTL_EVAL'{
-	     dest = Temp,
-	     value = #'RTL_ICON'{int = 0}
-	    },
-	  #'RTL_JUMP'{
-		       label = Testlbl
-		     },
-	  #'RTL_LABELDEF'{
-		       label = Bodylbl
-		      }] ++
-	ExprState#state.ir ++
-	[#'RTL_LABELDEF'{
-	    label = Testlbl
-	   },
-	 #'RTL_CJUMP'{
-		       relop = ne,
-		       temp1 = ExprState#state.previous_temp,
-		       temp2 = Temp
-		     }],
+             dest = Temp,
+             value = #'RTL_ICON'{int = 0}
+            },
+          #'RTL_JUMP'{
+                       label = Testlbl
+                     },
+          #'RTL_LABELDEF'{
+                       label = Bodylbl
+                      }] ++
+        ExprState#state.ir ++
+        [#'RTL_LABELDEF'{
+            label = Testlbl
+           },
+         #'RTL_CJUMP'{
+                       relop = ne,
+                       temp1 = ExprState#state.previous_temp,
+                       temp2 = Temp
+                     }],
     flatten(Tl, ExprState#state{ir = IR, previous_temp = []});
 
 flatten([#'RETURN'{expression = {void, _}}|Tl], State = #state{current_function = CF}) ->
@@ -295,29 +296,29 @@ flatten([#'RETURN'{expression = Expr}|Tl], State = #state{current_function = CF}
     RetTemp = lookup("__ret_" ++ CF, State),
 
     ExprState = flatten(Expr, State),
-    
-    IR = 
-	State#state.ir ++
-	ExprState#state.ir ++
-	[
-	 #'RTL_EVAL'{
-	    dest = RetTemp,
-	    value = ExprState#state.previous_temp
-	   },
-	 #'RTL_JUMP'{
-		      label = Endlbl
-		    }
-	],
-    
+
+    IR =
+        State#state.ir ++
+        ExprState#state.ir ++
+        [
+         #'RTL_EVAL'{
+            dest = RetTemp,
+            value = ExprState#state.previous_temp
+           },
+         #'RTL_JUMP'{
+                      label = Endlbl
+                    }
+        ],
+
     flatten(Tl, ExprState#state{ir = IR, previous_temp = []});
 
 flatten([#'EXTFUNC'{name = Name, return_type = ReturnType, formals = Formals}|Tl], State) ->
     FName = unwrap(Name),
     Size = sizeof(convert_type(ReturnType)),
-    
+
     %% We need to insert func-types
     %%    insert(extern_func, FName, convert_type(ReturnType), State),
-    
+
     flatten(Tl, State);
 
 flatten([#'FUNCTION'{name = Name, formals = Formals, return_type = ReturnType, locals = Locals, body = Body}|Tl], State) ->
@@ -333,14 +334,14 @@ flatten([#'FUNCTION'{name = Name, formals = Formals, return_type = ReturnType, l
 
     BodyState = flatten(Body, LocalState#state{current_function = unwrap(Name)}),
 
-    IR = State#state.ir ++ 
-	[#'RTL_PROC'{label = unwrap(Name), 
-		     framesize = FormalState#state.framesize + LocalState#state.framesize, 
-		     formals = FormalState#state.ir, 
-		     locals = LocalState#state.ir, 
-		     insns = BodyState#state.ir ++ [#'RTL_LABELDEF'{label = Endlbl}]
-		    }
-	],
+    IR = State#state.ir ++
+        [#'RTL_PROC'{label = unwrap(Name),
+                     framesize = FormalState#state.framesize + LocalState#state.framesize,
+                     formals = FormalState#state.ir,
+                     locals = LocalState#state.ir,
+                     insns = BodyState#state.ir ++ [#'RTL_LABELDEF'{label = Endlbl}]
+                    }
+        ],
     flatten(Tl, BodyState#state{ir = IR, current_function = []});
 
 flatten([{int_constant, _TokenLine, Value}], State) ->
@@ -354,10 +355,10 @@ flatten([H|T], State) ->
     flatten(T, State);
 flatten(P, State) when not is_list(P) ->
     flatten([P], State);
-flatten(A, B) ->
-    io:format("Died on input: ~p and state ~p", [A,B]).
+flatten(A, State) ->
+    io:format("Died on input: ~p and state ~p", [A,State]).
 
-    
+
 
 ast_to_rtl(#'PROGRAM'{decs = Decs, source = _Source}) ->
-    (flatten(Decs, #state{symboltable = dict:new()}))#state.ir.
+    {ok, (flatten(Decs, #state{symboltable = dict:new()}))#state.ir}.
